b474f581a3bdeec25aeedb89c69144497fa59ccf
Thomas Galliker <>
2024-08-13 14:54:14 +0200

    Add static Current to IBarcodeScanner

--- CameraScanner.Maui/Services/IBarcodeScanner.cs
+++ CameraScanner.Maui/Services/IBarcodeScanner.cs
@@ -3,6 +3,11 @@ namespace CameraScanner.Maui
 {
     public interface IBarcodeScanner
     {
+        /// <summary>
+        /// Gets the singleton instance of <see cref="IBarcodeScanner"/>.
+        /// </summary>
+        public static IBarcodeScanner Current { get; set; } = BarcodeScanner.Current;
+
         Task<HashSet<BarcodeResult>> ScanFromImageAsync(byte[] imageArray);
 
         Task<HashSet<BarcodeResult>> ScanFromImageAsync(FileResult file);
