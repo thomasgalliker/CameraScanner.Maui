90071c652e3e36a6f0fd6ca74389efd89dbe83dd
Thomas Galliker <>
2024-09-10 07:20:33 +0200

    Update navigation service in sample app

--- Samples/CameraDemoApp/Services/Navigation/MauiNavigationService.cs
+++ Samples/CameraDemoApp/Services/Navigation/MauiNavigationService.cs
@@ -20,42 +20,7 @@ namespace CameraDemoApp.Services.Navigation
         {
             try
             {
-                var pageTypes = FindTypesWithName(pageName);
-                if (pageTypes.Length == 0)
-                {
-                    throw new PageNavigationException($"Page with name '{pageName}' not found");
-                }
-
-                if (pageTypes.Length > 1)
-                {
-                    throw new PageNavigationException(
-                        $"Multiple pages found for name '{pageName}': " +
-                        $"{string.Join($"> {Environment.NewLine}", pageTypes.Select(t => t.FullName))}");
-                }
-
-                var pageType = pageTypes.Single();
-                var page = (Page)this.serviceProvider.GetRequiredService(pageType);
-
-                var viewModelName = pageName.Substring(0, pageName.LastIndexOf("Page")) + "ViewModel";
-                var viewModelTypes = FindTypesWithName(viewModelName);
-
-                if (viewModelTypes.Length == 0)
-                {
-                    this.logger.LogInformation($"View model with name '{viewModelName}' not found");
-                }
-                else if (viewModelTypes.Length == 1)
-                {
-                    var viewModelType = viewModelTypes.Single();
-                    var viewModel = this.serviceProvider.GetRequiredService(viewModelType);
-                    page.BindingContext = viewModel;
-                }
-                else
-                {
-                    throw new PageNavigationException(
-                        $"Multiple view models found for name '{viewModelName}': " +
-                        $"{string.Join($"> {Environment.NewLine}", viewModelTypes.Select(t => t.FullName))}");
-                }
-
+                var page = this.ResolvePage(pageName);
                 var navigation = GetNavigation();
                 await navigation.PushAsync(page);
             }
@@ -66,19 +31,90 @@ namespace CameraDemoApp.Services.Navigation
             }
         }
 
+        public async Task PushModalAsync(string pageName)
+        {
+            try
+            {
+                var page = this.ResolvePage(pageName);
+                var navigation = GetNavigation();
+                await navigation.PushModalAsync(new NavigationPage(page));
+            }
+            catch (Exception ex)
+            {
+                this.logger.LogError(ex, "PushModalAsync failed with exception");
+                throw;
+            }
+        }
+
+        private Page ResolvePage(string pageName)
+        {
+            var pageTypes = FindTypesWithName(pageName);
+            if (pageTypes.Length == 0)
+            {
+                throw new PageNavigationException($"Page with name '{pageName}' not found");
+            }
+
+            if (pageTypes.Length > 1)
+            {
+                throw new PageNavigationException(
+                    $"Multiple pages found for name '{pageName}': " +
+                    $"{string.Join($"> {Environment.NewLine}", pageTypes.Select(t => t.FullName))}");
+            }
+
+            var pageType = pageTypes.Single();
+            var page = (Page)this.serviceProvider.GetRequiredService(pageType);
+
+            var viewModelName = pageName.Substring(0, pageName.LastIndexOf("Page")) + "ViewModel";
+            var viewModelTypes = FindTypesWithName(viewModelName);
+
+            if (viewModelTypes.Length == 0)
+            {
+                this.logger.LogInformation($"View model with name '{viewModelName}' not found");
+            }
+            else if (viewModelTypes.Length == 1)
+            {
+                var viewModelType = viewModelTypes.Single();
+                var viewModel = this.serviceProvider.GetRequiredService(viewModelType);
+                page.BindingContext = viewModel;
+            }
+            else
+            {
+                throw new PageNavigationException(
+                    $"Multiple view models found for name '{viewModelName}': " +
+                    $"{string.Join($"> {Environment.NewLine}", viewModelTypes.Select(t => t.FullName))}");
+            }
+
+            return page;
+        }
+
         private static INavigation GetNavigation()
         {
+            if (Shell.Current != null)
+            {
+                throw new NotSupportedException(
+                    $"{nameof(MauiNavigationService)} does currently not support AppShell navigation");
+            }
+
             if (Application.Current?.MainPage is not Page page)
             {
                 throw new PageNavigationException("Application.Current.MainPage is not set");
             }
 
-            if (page is NavigationPage navigationPage)
-            {
-                page = navigationPage.CurrentPage;
-            }
+            var targetPage = GetTarget(page);
+            return targetPage.Navigation;
+        }
 
-            return page.Navigation;
+        private static Page GetTarget(Page target)
+        {
+            return target switch
+            {
+                FlyoutPage flyout => GetTarget(flyout.Detail),
+                TabbedPage tabbed => GetTarget(tabbed.CurrentPage),
+                NavigationPage navigation => GetTarget(navigation.CurrentPage) ?? navigation,
+                ContentPage page => page,
+                null => null,
+                _ => throw new NotSupportedException($"The page type '{target.GetType().FullName}' is not supported.")
+            };
         }
 
         private static Type[] FindTypesWithName(string typeName)
@@ -99,6 +135,34 @@ namespace CameraDemoApp.Services.Navigation
             catch (Exception ex)
             {
                 this.logger.LogError(ex, "PopAsync failed with exception");
+                throw;
+            }
+        }
+
+        public async Task PopToRootAsync()
+        {
+            try
+            {
+                var navigation = GetNavigation();
+                await navigation.PopToRootAsync();
+            }
+            catch (Exception ex)
+            {
+                this.logger.LogError(ex, "PopToRootAsync failed with exception");
+                throw;
+            }
+        }
+
+        public async Task PopModalAsync()
+        {
+            try
+            {
+                var navigation = GetNavigation();
+                await navigation.PopModalAsync();
+            }
+            catch (Exception ex)
+            {
+                this.logger.LogError(ex, "PopModalAsync failed with exception");
                 throw;
             }
         }
