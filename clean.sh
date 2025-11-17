16c775573e1025de1626cea9802347c268972251
Thomas Galliker <thomas@bluewin.ch>
2025-11-17 13:25:19 +0100

    Cleanup

--- clean.sh
+++ clean.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-#
+
+# Kill all mono-sgen processes
+pkill -9 -f mono-sgen
+
+# Remove compiled sources from bin/ob
 find . -type d -name "bin" -exec rm  -rf {} 2> /dev/null \;
 find . -type d -name "obj" -exec rm  -rf {} 2> /dev/null \;
-find . -name ".DS_Store" -delete
+
+# Remove Xcode build cache
+rm -rf ~/Library/Developer/Xcode/DerivedData
+
+# Remove Rider cache
+rm -rf .idea
